d60f8d4d4525fa0bed3f7ffcd17648493de23571
Fedor Sergeev <theodor.sergeev@yandex.ru>
2022-04-03 17:21:09 +0200

    fix duplicate files sdf load_sdf

--- src/load_sdf.py
+++ src/load_sdf.py
@@ -3,10 +3,21 @@ Data module for SDF samples.
 """
 
 import os, os.path
-
 import numpy as np
 
 
+# new version
+def load_shuffled_sdf_samples_from_file(filename, n_samples):
+    npz = np.load(filename)
+    samples = np.random.permutation(npz)[:n_samples]
+
+    xyz = samples[:, 0:3]
+    sdf = samples[:, 3:4]
+
+    return xyz, sdf
+
+
+# old version
 def load_samples_from_file(filename, n_samples):
     """
     Load samples from a file.
