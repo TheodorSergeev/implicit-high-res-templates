53e0258839dd585268cb2e7dcaeb4c3bfd731ffc
Fedor Sergeev <theodor.sergeev@yandex.ru>
2022-05-31 14:12:52 +0200

    add Earth Mover Distance metric

---
+++ src/emd.py
@@ -0,0 +1,11 @@
+from scipy.spatial.distance import cdist
+from scipy.optimize import linear_sum_assignment
+
+def emd(X_source, X_target):
+    d = cdist(X_source, X_target)
+    assignment = linear_sum_assignment(d)
+    return d[assignment].sum() / min(len(X_source), len(X_target))
+
+def emd_mesh(gt_surf_samples, mesh_target, N_target=10000):
+    sample_arr2 = sample_sdf_from_mesh_surface(mesh_target, N_target)[:,0:3]
+    return emd(gt_surf_samples, sample_arr2)
