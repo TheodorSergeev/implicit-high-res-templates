588b255340d8ee1b1f5ac7f7d702266744447a28
Fedor Sergeev <theodor.sergeev@yandex.ru>
2022-05-31 14:18:17 +0200

    fix import bug

--- src/emd.py
+++ src/emd.py
@@ -1,5 +1,6 @@
 from scipy.spatial.distance import cdist
 from scipy.optimize import linear_sum_assignment
+import sample_sdf
 
 def emd(X_source, X_target):
     d = cdist(X_source, X_target)
@@ -7,5 +8,5 @@ def emd(X_source, X_target):
     return d[assignment].sum() / min(len(X_source), len(X_target))
 
 def emd_mesh(gt_surf_samples, mesh_target, N_target=10000):
-    sample_arr2 = sample_sdf_from_mesh_surface(mesh_target, N_target)[:,0:3]
+    sample_arr2 = sample_sdf.sample_sdf_from_mesh_surface(mesh_target, N_target)[:,0:3]
     return emd(gt_surf_samples, sample_arr2)
